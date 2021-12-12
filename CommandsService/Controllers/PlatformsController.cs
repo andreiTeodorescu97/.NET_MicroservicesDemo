2bea2a6012be1e20df4cac17804c9687b1f8a938
andreiTeodorescu997 <andrei.teodorescu997@gmail.com>
2021-12-12 21:11:56 +0200

    Added one API POST

---
+++ CommandsService/Controllers/PlatformsController.cs
@@ -0,0 +1,23 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CommandsService.Controllers
+{
+    [Route("api/c/[controller]")]
+    [ApiController]
+    public class PlatformsController : ControllerBase
+    {
+        public PlatformsController()
+        {
+            
+        }
+
+        [HttpPost]
+        public ActionResult TestInboundConnection()
+        {
+            Console.WriteLine("--> Inboud POST # Commands Service");
+
+            return Ok("Test OK from Platforms Controller!");
+        }
+    }
+}
